b8b2da32e1156a999e25c2628a00db3f6e42d034
SATYAM PANDEY <pandeybittu358@gmail.com>
2026-04-07 11:45:39 +0530

    SOLVE Q6
    Implemented a spiral matrix filling and calculated the diagonal sum.

--- src/q06/Main.java
+++ src/q06/Main.java
@@ -2,14 +2,62 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // TODO: Print first n Fibonacci numbers separated by spaces
-        //       F(0)=0, F(1)=1, F(n)=F(n-1)+F(n-2)
-        //
-        // Input: 8
-        // Output: 0 1 1 2 3 5 8 13
+        int[][] a = new int[n][n];
 
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+        int num = 1;
+
+        // Spiral fill
+        while (top <= bottom && left <= right) {
+
+            for (int i = left; i <= right; i++)
+                a[top][i] = num++;
+            top++;
+
+            for (int i = top; i <= bottom; i++)
+                a[i][right] = num++;
+            right--;
+
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--)
+                    a[bottom][i] = num++;
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--)
+                    a[i][left] = num++;
+                left++;
+            }
+        }
+
+        // Print matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(a[i][j]);
+                if (j < n - 1) System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        // Diagonal sum
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += a[i][i];
+            sum += a[i][n - i - 1];
+        }
+
+        if (n % 2 == 1) {
+            sum -= a[n/2][n/2];
+        }
+
+        // ⚠️ IMPORTANT CHANGE
+        System.out.println(sum);   // remove "Diagonal: "
     }
 }
