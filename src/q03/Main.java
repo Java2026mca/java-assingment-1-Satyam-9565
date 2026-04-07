4d3f49cd25d05a3c4b3507019efaf807920b8a66
SATYAM PANDEY <pandeybittu358@gmail.com>
2026-04-07 11:07:30 +0530

    solve q3

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -3,9 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
+        int n = sc.nextInt();se spiral order starting from 1
         //       Print each row with values separated by single space
         //       Then print: "Diagonal: X" where X = sum of primary diagonal (top-left to bottom-right)
         //
@@ -18,3 +16,6 @@ public class Main {
 
     }
 }
+
+
+        // TODO: Fill an N×N matrix in clockwi
